7decabb648d7250337474458cf9544eaff421d75
monitari <qualified7389@gmail.com>
2024-12-29 14:04:34 +0900

    Commit Unity Project

---
+++ Assets/Scripts/Jelly.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Jelly
+{
+    public int ID;
+    public int Level;
+    public float Exp;
+
+    public Jelly(int ID, int Level, float Exp) {
+        this.ID = ID;
+        this.Level = Level;
+        this.Exp = Exp;
+    }
+}
